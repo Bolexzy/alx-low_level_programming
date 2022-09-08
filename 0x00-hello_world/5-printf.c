0d65cc2e37a120774f0ae26a22ad075f9dae54d3
Bolexzy <boluoyewumi79@gmail.com>
2022-09-08 12:17:43 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Description: program that prints exactly 
+ * with proper grammar, but the outcome is a piece of art,
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
