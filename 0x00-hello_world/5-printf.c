f83d861ae791ae309c87664e595b07a9d8dd8690
Bolexzy <boluoyewumi79@gmail.com>
2022-09-08 12:24:43 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,6 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * Description: program that prints exactly 
- * with proper grammar, but the outcome is a piece of art,
  */
 int main(void)
 {
