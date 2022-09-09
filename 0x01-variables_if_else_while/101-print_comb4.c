56f183ec3724b3f9447ed9f82db3e60d9bed444f
Bolexzy <boluoyewumi79@gmail.com>
2022-09-09 15:08:53 -0700

    Add 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,14 +13,14 @@ int main(void)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			for (k = '0'; k<= '9'; k++)
+			for (k = '0'; k <= '9'; k++)
 			{
 				if (!(k <= j || j <= i))
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (!( i == '7' && j == '8' && k == '9'))
+					if (!(i == '7' && j == '8' && k == '9'))
 					{
 						putchar(',');
 						putchar(' ');
