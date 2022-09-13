5692387011e70a72ccf844a5fd1dea48fd1cea77
Bolexzy <boluoyewumi79@gmail.com>
2022-09-13 10:36:47 -0700

    Add 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* print_to_98 - Prints all numbers from n to 98.
+*
+* @n: input number.
+*
+* Return: no return.
+*/
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+
+}
+
