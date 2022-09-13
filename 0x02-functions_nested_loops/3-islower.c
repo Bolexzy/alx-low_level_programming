bea51abbb2c132a4d92b4c699978e2a29aa96a4c
Bolexzy <boluoyewumi79@gmail.com>
2022-09-13 07:08:01 -0700

    Add 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _islower - Shows 1 if the input is a
+* lowercase character. Another cases, shows 0
+*
+* @c: The character in ASCII code
+*
+* Return: 1 for lowercase character. 0 fpr the rest.
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	_putchar('\n');
+	return (0);
+}
