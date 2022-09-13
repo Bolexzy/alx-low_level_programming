18c70107abfce47ba564f2964240c5ed0506b2cf
Bolexzy <boluoyewumi79@gmail.com>
2022-09-13 09:03:51 -0700

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* jack_bauer - Prints every minute of the day.
+*
+* Return: no return.
+*/
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	/* 00:00 to 23:59 */
+	for (i = '0'; i < '3'; i++)
+	{
+		for (j = '0'; j < '4'; j++)
+		{
+			for (k = '0'; k < '6'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+
+	}
+}
