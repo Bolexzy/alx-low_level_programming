3afba90111585656b9e0c816e83a8f268e288b20
Bolexzy <boluoyewumi79@gmail.com>
2022-09-15 15:53:42 -0700

    Add 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,10 +14,10 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (s = 1; s < (size - i); s++)
-					_putchar(' ');
+				_putchar(' ');
 			for (j = 0; j <= i; j++)
- 			{ 
-			_putchar('#');
+			{
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
