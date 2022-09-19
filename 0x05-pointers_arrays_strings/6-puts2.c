d7b19d9da55e803e04922b0ce81cedaf9d5662df
Bolexzy <boluoyewumi79@gmail.com>
2022-09-19 16:20:48 -0700

    Add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts2 - prints one char out of 2 of a string.
+* @str: input string.
+* Return: no return.
+*/
+void puts2(char *str)
+{
+	int len = 0, count = 0, i;
+
+	while (str[count++])
+		len++;
+
+	for (i = 0; i < (len - 1); i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
