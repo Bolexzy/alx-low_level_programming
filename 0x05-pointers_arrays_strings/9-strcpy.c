1d80977eae4a135708952386406283ed2187fed4
Bolexzy <boluoyewumi79@gmail.com>
2022-09-19 17:40:34 -0700

    Add 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,7 +15,7 @@ char *_strcpy(char *dest, char *src)
 	while (src[index])
 	{
 		*(dest + index) = *(src + index);
-		if (*(src + count) == '\0')
+		if (*(src + index) == '\0')
 			break;
 		index++;
 	}
