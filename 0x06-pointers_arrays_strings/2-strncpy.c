b2f0e98dba659d67b9af680dc48e9b9672296177
Bolexzy <boluoyewumi79@gmail.com>
2022-09-21 18:27:07 +0100

    Add 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,20 +10,12 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0;
+	int i;
 
-	while (index < n)
-	{
-		if (*(src + index) != '\0')
-		{
-			*(dest + index) = *(src + index);
-			index++;
-		}
-		else
-		{
-			*(dest + index) = '\0';
-			index++;
-		}
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
