7c6ac063069f4b4f23234539ae289b96356441af
Bolexzy <boluoyewumi79@gmail.com>
2022-09-26 23:52:05 +0100

    Add 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -20,7 +20,7 @@ char *_strstr(char *haystack, char *needle)
 			if (*(haystack + (i + j)) != *(needle + j))
 				break;
 		}
-		if (needle[j] == '\0')
+		if (!needle[j])
 			return (haystack + i);
 	}
 	return ('\0');
