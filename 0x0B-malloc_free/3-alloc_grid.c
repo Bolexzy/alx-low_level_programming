43563dcbad688e04f45a0062ac187ffb3306191b
Bolexzy <boluoyewumi79@gmail.com>
2022-10-03 17:34:06 +0100

    Add 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
+ *
+ * Return: pointer of an array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			free(grid[i]);
+			return (NULL);
+		}
+	}
+
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -17,4 +17,4 @@ Filename | Description
 `0-create_array.c` | Creates an array of chars, and initializes it with a specific char.
 `1-strdup.c` | Returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
 `2-str_concat.c` | Concatenates two strings.
-
+`3-alloc_grid.c` | Returns a pointer to a 2 dimensional array of integers
