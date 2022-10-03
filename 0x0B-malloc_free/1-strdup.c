9ba4bed4d6ae9d71adaa37f518bf618f11f5853c
Bolexzy <boluoyewumi79@gmail.com>
2022-10-03 13:23:32 +0100

    Fix 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,20 +11,18 @@
 char *_strdup(char *str)
 {
 	char *s;
-	int i, j;
+	unsigned int i, j;
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	s = malloc((i + 1) * sizeof(char));
+	s = (char *)malloc((i + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
 
 	for (j = 0; j < i; j++)
-	{
 		s[j] = str[j];
-	}
 
 	return (s);
 }
