35085959e4785850f7f79870fa7ddda54ca3bd28
Bolexzy <boluoyewumi79@gmail.com>
2022-10-07 00:31:35 +0100

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,19 +18,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
 	if (n > j)
 		n = j;
-
 	str = malloc(sizeof(char) * (i + n + 1));
 
 	if (str == NULL)
