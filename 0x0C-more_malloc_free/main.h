5bd49aa9f743bbf0379059c2863c447929d0075f
Bolexzy <boluoyewumi79@gmail.com>
2022-10-07 01:27:27 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
