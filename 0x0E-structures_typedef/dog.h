85e1a257a4425ed0144b49218402d80f89028a85
Bolexzy <boluoyewumi79@gmail.com>
2022-10-10 17:06:01 +0100

    Add type for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG_H
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - Struct to store information of a dog.
  *
  * @name: name of the dog
@@ -11,12 +16,12 @@
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
