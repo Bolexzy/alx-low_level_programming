8ad667b9f56b935e4f283f769fdda2a4cd6fa0ff
Bolexzy <boluoyewumi79@gmail.com>
2022-10-10 16:52:23 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,6 @@ typedef struct dog
 	char *owner;
 } dog;
 
-init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
