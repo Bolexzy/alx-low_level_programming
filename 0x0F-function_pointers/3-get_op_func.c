c0cbc063a2041d6dadbc3fb97cac48f9f417c41d
Bolexzy <boluoyewumi79@gmail.com>
2022-10-13 15:50:11 +0100

    Fix 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,11 +19,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (ops[i].op)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
