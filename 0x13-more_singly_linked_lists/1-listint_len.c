d8477bbfe32fea6088ef781bf3f8880a7cc196bc
Bolexzy <boluoyewumi79@gmail.com>
2022-10-24 20:58:12 +0100

    Add 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in
+ * a linked list.
+ *
+ * @h: Head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes;
+
+	while (h)
+	{
+		h = h->next;
+
+		nodes++;
+	}
+	return (nodes);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -16,3 +16,4 @@ All of the following are programs written in C:
 Filename | Description
 --- | ---
 `0-print_listint.c` | Prints all the elements of a `listint_t` list.
+`1-listint_len.c` | Returns the number of elements in a linked `listint_t` list.
