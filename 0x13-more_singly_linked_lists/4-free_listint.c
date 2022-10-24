ce5563d482c4af9c37362fdf29f2592bdaab1b43
Bolexzy <boluoyewumi79@gmail.com>
2022-10-24 22:09:36 +0100

    Fix 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,7 +8,7 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t tmp;
+	listint_t *tmp;
 
 	while (head)
 	{
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,10 +12,7 @@ void free_listint2(listint_t **head)
 	listint_t *tmp;
 
 	if (head == NULL)
-	{
-		free(*head);
 		return;
-	}
 
 	while (*head)
 	{
