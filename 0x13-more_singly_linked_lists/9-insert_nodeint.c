310a57f6536d135db8c0844547bb0ea55ea966d4
Oyewumi Boluwatife Emmanuel <74738413+Bolexzy@users.noreply.github.com>
2022-10-26 02:36:53 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	i = 0;
-	while (i < (idx - 1))
+	while (i < (idx - 1) && h != NULL)
 	{
 		h = h->next;
 		i++;
