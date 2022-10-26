4118e6bff707e85e8858a83747804eb353204f2a
Bolexzy <boluoyewumi79@gmail.com>
2022-10-27 00:59:23 +0100

    Fix 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,15 +8,15 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	const listint_t *next;
-	const listint_t *prev;
+	listint_t *next;
+	listint_t *prev;
 
 	prev = NULL;
 
-	while (current != NULL)
+	while (*head != NULL)
 	{
 		next = (*head)->next;
-		*(head)->next = prev;
+		(*head)->next = prev;
 		prev = *(head);
 		*(head) = next;
 	}
