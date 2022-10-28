4c68f5919c434404031a51c120478f63264f0fb5
Bolexzy <boluoyewumi79@gmail.com>
2022-10-28 01:24:29 +0100

    Add 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,12 +7,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n)
+	if (n >> 0)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
 	}
 	else
+	{
 		_putchar('0');
+	}
 }
