21713fd8cda5f5ca67f393afd7db94c378b8833b
Bolexzy <boluoyewumi79@gmail.com>
2022-10-28 03:30:53 +0100

    Add 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,10 +10,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == 0 && index > 63)
+	if (n == 0 || index > 63)
 		return (-1);
 
-	*n = (*n) | (1 << index);
+	*n = (*n | (1 << index));
 
 	return (1);
 }
