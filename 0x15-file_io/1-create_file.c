d30e054cd2b8adffa036d8f423c9df8888e7d492
Bolexzy <boluoyewumi79@gmail.com>
2022-11-01 02:27:56 +0100

    Fix 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len]; len++)
 		;
 
-	bytes = write(STDOUT_FILENO, text_content, len);
+	bytes = write(fd, text_content, len);
 
 	if (bytes < 0)
 		return (-1);
