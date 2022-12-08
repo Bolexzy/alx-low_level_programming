9744eeb0228a1f2c5125adcaece70672ec15fede
Bolexzy <boluoyewumi79@gmail.com>
2022-12-08 18:58:07 +0100

    Add 3-add_dnodeint_end.c adds a new node at the end of a dlistint_t list.

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The value of the element for new node;
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *h = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (h != NULL)
+	{
+		while (h != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	new->prev = h;
+	return (new);
+}
