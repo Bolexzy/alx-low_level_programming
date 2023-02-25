97291f7e2ca7163bbb25a1fd6c6f39baccf310e9
Bolexzy <boluoyewumi79@gmail.com>
2023-02-26 00:44:52 +0100

    Update code: fix error

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,20 +13,22 @@ def island_perimeter(grid):
     """
     height, width = 0, 0
 
-    ncols = len(grid[0])
-    index = 0
-    for r in range(len(grid)):
-        for i in range(ncols):
-            if grid[r][i] == 1:
-                index = i
-                break
-        if index != 0:
-            if grid[r][index] != 0:
-                height += 1
-            if r + 1 >= len(grid) or grid[r + 1][index] == 0:
-                while grid[r][index] != 0:
-                    width += 1
-                    index += 1
+    num_rows = len(grid)
+    num_cols = len(grid[0])
 
-    if height and width:
-        return (2 * (height + width))
+    for r in range(num_rows):
+        for c in range(num_cols):
+            if grid[r][c] == 1:
+                # Count the top and bottom sides of the island
+                if r == 0 or grid[r-1][c] == 0:
+                    height += 1
+                if r == num_rows-1 or grid[r+1][c] == 0:
+                    height += 1
+
+                # Count the left and right sides of the island
+                if c == 0 or grid[r][c-1] == 0:
+                    width += 1
+                if c == num_cols-1 or grid[r][c+1] == 0:
+                    width += 1
+
+    return (height + width)
